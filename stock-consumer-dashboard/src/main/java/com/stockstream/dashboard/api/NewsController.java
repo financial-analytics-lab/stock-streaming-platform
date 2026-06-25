18fe7aef8f48ef7062e65eb61515fa03a6d0f950
abdallahadel667 <abdallahadel667@gmail.com>
2026-06-25 13:47:09 +0300

    fix news loader

--- stock-consumer-dashboard/src/main/java/com/stockstream/dashboard/api/NewsController.java
+++ stock-consumer-dashboard/src/main/java/com/stockstream/dashboard/api/NewsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
